4c29710b57ba1188a5893693ee0ff9743ae4ab6f
Hayden1807 <hayden.guenon7@gmail.com>
2024-09-25 19:38:35 +0200

    Update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Guelap" },
+            { "message": "Salut je suis du code.", "author": "Guelap" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Guelap" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
